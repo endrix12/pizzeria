8ebd1e1b38ab7b5fb3163e89773af5095281012f
Andrzej Buśko <endrix12@wp.pl>
2021-10-11 15:54:10 +0200

    Operacje CRUD na użytkownikach

--- src/main/java/pl/abusko/pizzeria/User/UserController.java
+++ src/main/java/pl/abusko/pizzeria/User/UserController.java
@@ -4,10 +4,11 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import java.util.List;
-
 
 @Controller
 @AllArgsConstructor
@@ -19,15 +20,23 @@ public class UserController {
     @GetMapping("register")
     public String register(Model model){
         model.addAttribute("newUserDto", new UserRegDto());
+        model.addAttribute("userExists",Boolean.FALSE);
         return "register";
     }
 
     @PostMapping("register")
-    public String registerUser(@ModelAttribute UserRegDto userRegDto){
-
-        userRegDto.setPassword(passwordEncoder.encode(userRegDto.getPassword()));
-        userService.add(userRegDto.toUserEntity());
-        return "create";
+    public String registerUser(@ModelAttribute UserRegDto userRegDto,Model model) {
+      Boolean existsUser = userService.existsUserByEmail(userRegDto.getEmail());
+        if (existsUser) {
+            model.addAttribute("newUserDto", userRegDto);
+            model.addAttribute("userExists",Boolean.TRUE);
+            return "register";
+        }
+         else{
+            userRegDto.setPassword(passwordEncoder.encode(userRegDto.getPassword()));
+            userService.add(userRegDto.toUserEntity());
+            return "create";
+        }
     }
 
     @GetMapping("admin")
@@ -37,11 +46,17 @@ public class UserController {
         return "admin-panel";
     }
 
-    @DeleteMapping("delete/{id}")
+    @GetMapping("delete/{id}")
     public String deleteUser(@PathVariable(name="id") Long id){
         userService.deleteUserById(id);
         return "redirect:/admin";
     }
 
+    @GetMapping("update/{id}")
+    public String updateUser(@PathVariable(name="id") Long id, Model model){
+        User user = userService.findUserById(id);
+        model.addAttribute("newUserDto",user.toDto());
+        return "register";
+    }
 
 }
